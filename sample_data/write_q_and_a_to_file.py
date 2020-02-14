b23739ba57000f22a2eeaf5170ad36cdddcd7d4c
Gheboianu Bogdan <bogdan.gheboianu.2013@gmail.com>
2020-02-14 11:42:05 +0200

    Some refactoring in write_q_and_a_to_file.py

--- sample_data/write_q_and_a_to_file.py
+++ sample_data/write_q_and_a_to_file.py
@@ -37,37 +37,31 @@ def organize_qas(qas):
 def create_list_of_dicts_info(qas_dict):
     all_questions = []
     all_answers = []
-    question_count = 5
-    answer_count = 12
+    question_count = 0
+    answer_count = 0
     for question, answer in qas_dict.items():
         # for questions:
         question_dict = {}
-        question_id = question_count
         question_splitted = question.split()
-        question_title = question_splitted[0] + " " + question_splitted[1] + "..."
-        question_message = question
-        question_submission_time = int(calendar.timegm(time.gmtime())) + 7200
-        question_vote_number = "0-0"
-        question_view_number = "0"
-        question_image = ""
-        question_dict.update({"id": question_id, "submission_time": question_submission_time, "view_number": question_view_number, 
-                                "vote_number": question_vote_number, "title": question_title, "message": question_message, "image": question_image})
+        question_dict.update({"id": question_count, 
+                            "submission_time": int(calendar.timegm(time.gmtime())) + 7200, 
+                            "view_number": "0", 
+                            "vote_number": "0-0", 
+                            "title": question_splitted[0] + " " + question_splitted[1] + "...", 
+                            "message": question, 
+                            "image": ""})
         # for answers:
         answer_dict = {}
-        answer_id = answer_count
-        answer_submission_time = int(calendar.timegm(time.gmtime())) + 7200000
-        answer_vote_number = "0-0"
-        answer_question_id = question_id
-        answer_message = answer
-        answer_image = ""
-        answer_dict.update({"id": answer_id, "submission_time": answer_submission_time, "vote_number": answer_vote_number, 
-                                "question_id": answer_question_id, "message": answer_message, "image": answer_image})
-
+        answer_dict.update({"id": answer_count, 
+                            "submission_time": int(calendar.timegm(time.gmtime())) + 7200000, 
+                            "vote_number": "0-0", 
+                            "question_id": question_count, 
+                            "message": answer, 
+                            "image": ""})
         all_questions.append(question_dict)
         all_answers.append(answer_dict)
         answer_count += 1
         question_count += 1
-
     return all_questions, all_answers
 
 
@@ -88,9 +82,6 @@ def write_answers_to_file(all_answers):
             writer.writerow(question)
         file.close()
 
-
-create_list_of_dicts_info(organize_qas(extract_data()))
-    
 
 def main():
     ALL_QUESTIONS = 0
